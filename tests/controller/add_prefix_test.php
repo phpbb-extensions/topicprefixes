9954a9cc73923c9870490f5b39f7ff4383e36756
Matt Friedman <maf675@gmail.com>
2024-01-18 10:24:06 -0800

    Simplify test code
    Signed-off-by: Matt Friedman <maf675@gmail.com>

--- tests/controller/add_prefix_test.php
+++ tests/controller/add_prefix_test.php
@@ -54,8 +54,8 @@ class add_prefix_test extends admin_controller_base
 			if (!$valid_form)
 			{
 				// Throws E_WARNING in PHP 8.0+ and E_USER_WARNING in earlier versions
-				$exceptionName = version_compare(PHP_VERSION, '8.0', '<') ? \PHPUnit\Framework\Error\Error::class : \PHPUnit\Framework\Error\Warning::class;
-				$errno = version_compare(PHP_VERSION, '8.0', '<') ? E_USER_WARNING : E_WARNING;
+				$exceptionName = PHP_VERSION_ID < 80000 ? \PHPUnit\Framework\Error\Error::class : \PHPUnit\Framework\Error\Warning::class;
+				$errno = PHP_VERSION_ID < 80000 ? E_USER_WARNING : E_WARNING;
 				$this->expectException($exceptionName);
 				$this->expectExceptionCode($errno);
 			}
